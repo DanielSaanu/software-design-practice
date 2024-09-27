315dcca617b3356379541004d18ed1571421972a
DanielSaanu <danielsaanu01@gmail.com>
2024-09-27 16:44:45 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Reversi {
+public:
+    void printRevese() {
+
+    }
+};
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "Number: " << number << endl;
+
+    Reversi re;
+    re.printRevese();
+
+
+    return 0;
+}
